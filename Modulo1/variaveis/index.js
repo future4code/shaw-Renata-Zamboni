63028632fe1a71b9c03a32eac0afa8d3109f548b
RenataSZamboni <rnt.zamboni@gmail.com>
2022-02-15 14:48:18 -0300

    salvando variaveis

---
+++ Modulo1/variaveis/index.js
@@ -0,0 +1,66 @@
+console.log ("Hello world")
+
+/*
+exercício 1
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+*/
+
+//10
+//10,5
+
+
+/*
+exercício 2
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+*/
+
+//10, 10, 10
+
+
+/*
+exercício 4
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+
+let horasTrabalhoDia = prompt("Quantas horas você trabalha por dia?")
+let salario = prompt("Quanto você recebe por dia?")
+alert("Voce recebe ${salario/horasTrabalhoDia} por hora")
+*/
+
+//Exercício de escrita de código 1
+
+let nome
+console.log (typeof nome)
+
+let idade
+console.log(typeof idade)
+
+//As duas respostas deram "undefined", pois não há valor atribuido.
+
+let nomeUsuario = prompt("Qual é o seu nome?")
+let idadeUsuario = prompt("Qual a sua idade?")
+
+/*Na tela do navegador eu recebi,
+como mensagem popup com área para resposta,
+as perguntas "Qual é o seu nome?" e "Qual é a sua idade?", respectivamente.*/
+
+console.log("Olá", nomeUsuario,", você tem", idadeUsuario, "anos.");
+
+//Exercício escrita código 2
+
+
+
