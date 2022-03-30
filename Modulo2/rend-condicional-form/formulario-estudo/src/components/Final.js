8ae52a80e77bc73460e047c803bec22450484d93
RenataSZamboni <rnt.zamboni@gmail.com>
2022-03-30 00:01:07 -0300

    exercicios rend condicionais até 16

---
+++ Modulo2/rend-condicional-form/formulario-estudo/src/components/Final.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Final extends React.Component {
+  render() {
+    return (
+
+      <div className="final">
+
+        <h1>O questionário acabou</h1>
+        <h3>Muito obrigada por participar!</h3>
+                
+      </div>
+    );
+  }
+}
+  
+export default Final;
+  
