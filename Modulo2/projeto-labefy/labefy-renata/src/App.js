0387e340a0f790af95ee5c946ba9ef7270b0d72c
Renata S. Zamboni <86760783+RenataSZamboni@users.noreply.github.com>
2022-04-07 22:49:44 -0300

    escopo projeto + dados api

---
+++ Modulo2/projeto-labefy/labefy-renata/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+
+export default class App extends React.Component{
+  
+  render(){
+
+
+    return (
+
+      <div>
+       
+      </div>
+
+    );
+
+
+  }
+  
+}
