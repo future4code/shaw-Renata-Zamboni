c3ee4cf4809351fb7a911e21e77d12d49f7ce460
Renata S. Zamboni <86760783+RenataSZamboni@users.noreply.github.com>
2022-04-24 23:54:43 -0300

    tentando botão match

--- Modulo3/projeto-astromatch/astromatch-renata/src/components/Header.js
+++ Modulo3/projeto-astromatch/astromatch-renata/src/components/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from 'styled-components';
 
-
 const HeaderContainer = styled.div`
     height: 40px;
     display: flex;
--- Modulo3/projeto-astromatch/astromatch-renata/src/pages/TelaInicial.js
+++ Modulo3/projeto-astromatch/astromatch-renata/src/pages/TelaInicial.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import axios from "axios";
 import { CardPerfil } from "../components/CardPerfil";
 import { Header } from "../components/Header";
+import iconeDislike from '../src/assets/dislike.png';
+import iconeLike from '../src/assets/like.png';
 
 const BotaoLista = styled.div`
     height: 30px;
@@ -11,8 +13,44 @@ const BotaoLista = styled.div`
     border-radius: 50%;
    
 `
+const IconeMatch = styled.div`
+    width: 30px;
+    margin-left: 10px;
+    border-radius: 50%;   
+`
+
+const IconeDeuRuim = styled.div`
+    width: 30px;
+    margin-left: 10px;
+    border-radius: 50%;
+   
+`
 
 export const TelaInicial=(props)=> {
+
+  const [like,setLike]=useState(false);
+
+  const [dislike, setDislike]=useState(false);
+
+  const onClickCurtida = () => {
+    if (like) {
+      setLike(false);      
+    } else {
+      setLike(true);      
+    }
+  };
+
+  const onClickDislike= () => {
+    if (dislike) {
+      setDislike(false);      
+    } else {
+      setDislike(true);      
+    }
+  };
+
+  const iconeLike = like? iconeLike: iconeLike;
+
+  const iconeDislike = dislike? iconeDislike: iconeDislike
 
   return (
     <TelaInicial>
@@ -26,6 +64,20 @@ export const TelaInicial=(props)=> {
       </Header>
       
       <CardPerfil/>
+
+      <PostFooter>
+
+        <IconeMatch
+          icone={iconeLike}
+          onClickIcone={onClickCurtida}
+        />
+
+        <IconeDeuRuim
+          icone={iconeDislike}
+          onClickIcone={onClickDislike}
+        />
+       
+      </PostFooter>
       
     </TelaInicial>
   );
