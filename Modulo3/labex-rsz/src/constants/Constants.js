317dc4dd8a6dba9bc68abeea95bbaad87bc48e35
Renata S. Zamboni <86760783+RenataSZamboni@users.noreply.github.com>
2022-04-28 20:25:57 -0300

    pagina funcional, mas problema em hook react country select

--- Modulo3/labex-rsz/src/constants/Constants.js
+++ Modulo3/labex-rsz/src/constants/Constants.js
@@ -1,3 +1,5 @@
 export const urlBase="https://us-central1-labenu-apis.cloudfunctions.net/labeX/renata-seabra-zamboni-shaw";
 
 export const headers="renata-seabra-zamboni-shaw"
+
+
--- Modulo3/labex-rsz/src/pages/ApplicationFormPage.js
+++ Modulo3/labex-rsz/src/pages/ApplicationFormPage.js
@@ -4,14 +4,99 @@ import styled from "styled-components";
 
 import {useNavigate} from "react-router-dom";
 import { goBack } from "../routes/Coordinator";
+import { headers, urlBase} from "../constants/Constants";
+
+import CountrySelect from 'react-bootstrap-country-select';
 
 const ApplicationFormPageBox =styled.div`
 `
 
 
+
 export function ApplicationFormPage() {
+
     const navigate = useNavigate()
+
+    //mesma lógica do estado de ListTripPage, mas com ID
+    const [getTrip, setGetTrip]= useState([])
+    const [id, setId] = useState("")
+
+    //post + um state pra cada item do body que recebe um input
+    const [postApply, setPostApply]= useState([])
+    const [nome, setNome] = useState("")
+    const [idade, setIdade] = useState(Number)
+    const [profissao, setProfissao] = useState("")
+    const [textoCandidatura, setTextoCandidatura] =  useState("")    
+    //const [pais, setPais] = useState([])
+    //react country select
+    const [value, setValue] = React.useState(null)      
+   
     
+   //useEffect+ axios para GET TRIP
+    useEffect(()=>{
+        
+        axios.get(`${urlBase}/trips`)
+        .then((response)=>{
+            setGetTrip(response.data.trips);   
+        }).catch((err)=>{
+            alert(err.data.response);
+        }) 
+
+    },[])
+
+    const showList = getTrip.map((trip)=>{
+        return(
+            <option key={trip.id} value={trip.id}>
+                {trip.name}
+            </option>
+        );
+    })
+    console.log(id)
+
+    const applyToTrip =()=>{
+        const body={
+            name: nome,
+            age: idade,
+            applicationText: textoCandidatura,
+            profession: profissao,
+            country: pais
+        }
+        console.log(body)
+
+       axios.post(`${urlBase}/trips/${id}/apply`, body)
+
+      .then((response)=>{
+       setPostApply(response.data.apply);
+        
+       }).catch((err)=>{
+            alert(err.data.response);
+       })
+    } 
+
+  //OnChange pra cada input
+  const onChangeId = (event)=>{
+    setId(event.target.value)
+  }
+
+  const onChangeNome = (event)=>{
+      setNome(event.target.value)
+    }
+
+  const onChangeIdade=(e)=>{
+      setIdade(e.target.value)
+    }
+
+  const onChangeProfissao=(e)=>{
+      setProfissao(e.target.value)
+  }
+
+  const onChangeTextoCandidatura = (e)=>{
+    setTextoCandidatura (e.target.value)
+  }
+
+  /*const onChangePais=(event)=>{
+      setPais(event.target.value)
+  }*/
     
 
   return (
@@ -22,29 +107,31 @@ export function ApplicationFormPage() {
 
         <h1>Formulário de inscrição</h1>
 
-        {/*passo 2 - map em get trip*/}
+        {/*passo 2 - map em get trip, mas com ID*/}
         <form>
 			<label>
                 Escolha uma viagem:
                 <br/>				
-				<select>
-					<option value="">Lista vinda de getTrips</option>						
+				<select onChange={onChangeId}>
+					{showList}						
 				</select>
 			</label>
 		</form>
         
         {/* passo 2.1 - inputs de formulario - post Apply to trip */}
         <br/>
-        <input placeholder="Nome"type="text"></input><br/>
-        <br/><input placeholder="Idade" type="text"></input><br/>
-        <br/><input placeholder="Profissão" type="text"></input><br/>
+        <input placeholder="Nome"type="text" onChange={onChangeNome}></input><br/>
+
+        <br/><input placeholder="Idade" type="number" onChange={onChangeIdade}></input><br/>
+
+        <br/><input placeholder="Profissão" type="text" onChange={onChangeProfissao}></input><br/>
               
         <br/>
         <form>
             <label>
                 Qual sua motivação para a viagem?      
                 <br/>
-                <textarea/>
+                <textarea onChange={onChangeTextoCandidatura}/>
             </label>
         </form>
 
@@ -54,14 +141,12 @@ export function ApplicationFormPage() {
             <label>
                 Em qual país reside?
                 <br/>			
-                <select>
-                    <option value="">Lista de países</option>						
-                </select>
+                <CountrySelect value={value} onChange={setValue}/>
             </label>
         </form>
 
         <br/>
-        <button>Enviar Inscrição</button>
+        <button onClick={applyToTrip}>Enviar Inscrição</button>
        
        
     </ApplicationFormPageBox>
