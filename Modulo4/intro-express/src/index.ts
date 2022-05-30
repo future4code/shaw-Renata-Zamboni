d66a26f27fc91224608892dc4424ae49f610eaa8
Renata S. Zamboni <86760783+RenataSZamboni@users.noreply.github.com>
2022-05-30 19:31:18 -0300

    exercicios intro-express

--- Modulo4/intro-express/src/index.ts
+++ Modulo4/intro-express/src/index.ts
@@ -1,11 +1,78 @@
-import express, {Express} from 'express';
+import express, {application, Express} from 'express';
 import cors from 'cors';
 import { AddressInfo } from "net";
+import { users } from './userData';
+import { posts } from './postData';
 
 const app: Express = express();
 
 app.use(express.json()); //middleware
 app.use(cors()); //middleware
+
+app.get("/", (req, res) => {          
+   res.send("Hello from Express")
+})
+
+//2
+// endpoints que buscam usuarios
+// GET /users - vem todos, com todas infos
+// GET /users/1 - todas infos do primeiro
+
+// app.get("/users", (req, res)=>{
+//    res.send("Lista de usuarios")
+// })
+
+//criar uma variavel de tipo
+type UserInfo = {
+   id: number,
+   name: string,
+   phone: number | string,
+   email: any,
+   website: any
+}
+
+//3 array de usuarios em data.ts
+
+//4
+app.get("/users", (req, res)=>{
+   const myUsers = users.map((user)=>{
+      return users
+   })
+   res.send(myUsers)
+})
+
+//5
+type Posts = {
+   userId: number,
+   id: number,
+   title: string,
+   body: string   
+}
+
+//6 array de posts em postData.ts
+//achei melhor criar fora do array de usuários pra 1. separar as infos por categoria, 2. não fazer um "inception" de arrays de objetos
+
+//7
+app.get("/posts", (req, res)=>{
+   const postList = posts.filter((post)=>{
+      return posts
+   })
+   res.send(postList)
+})
+
+//8 aaaaaah sos
+// app.get("posts/:userId",(req, res)=>{
+//    const postId = req.params.postId;
+
+//    const getUserPost = posts.filter((post)=>{
+//       if (post.id === postId){
+//             return post
+//          }
+//       })      
+   
+//    res.send(getUserPost)
+// })
+
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
---
+++ Modulo4/intro-express/src/userData.ts
@@ -0,0 +1,51 @@
+export const users = [
+    {
+        id: 1,
+        name: "Leanne Graham",
+        phone: "1-770-736-8031 x56442",
+        email: "Sincere@april.biz",
+        website: "hildegard.org"
+    },
+    {
+        id: 2,
+        name: "Ervin Howell",
+        phone: "010-692-6593 x09125",
+        email: "Shanna@melissa.tv",
+        website: "anastasia.net"
+    },
+    {
+        id: 3,
+        name: "Clementine Bauch",
+        phone: "1-463-123-4447",
+        email: "Nathan@yesenia.net",
+        website: "ramiro.info"
+    },
+    {
+        id: 4,
+        name: "Patricia Lebsack",
+        phone: "493-170-9623 x156",
+        email: "Julianne.OConner@kory.org",
+        website: "kale.biz"
+    },
+    {
+        id: 5,
+        name: "Chelsey Dietrich",
+        phone: "(254)954-1289",
+        email: "Lucio_Hettinger@annie.ca",
+        website: "demarco.info"
+    },
+    {
+        id: 6,
+        name: "Mrs. Dennis Schulist",
+        phone: "1-477-935-8478 x6430",
+        email: "Karley_Dach@jasper.info",
+        website: "ola.org",
+    },
+    {
+        id: 7,
+        name: "Kurtis Weissnat",
+        phone: "210.067.6132",
+        email: "Telly.Hoeger@billy.biz",
+        website: "elvis.io",
+    }
+]
