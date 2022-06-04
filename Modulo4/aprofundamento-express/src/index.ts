07965a6c332b017db10b973e5e98a702d35d5179
Renata S. Zamboni <86760783+RenataSZamboni@users.noreply.github.com>
2022-06-04 19:57:11 -0300

    revisão exercicios

--- Modulo4/aprofundamento-express/src/index.ts
+++ Modulo4/aprofundamento-express/src/index.ts
@@ -1,33 +1,73 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { AddressInfo } from "net";
-import { tarefas } from "./tarefas";
+import { tarefas, ToDo } from "./tarefas";
 
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+//1 teste localhost
 app.get("/ping", (req, res) => {          
     res.send("Pong! 🏓")
 })
 
-//2
-export type ToDo ={
-    userId: number,
-    id: number,
-    title: string,
-    completed: boolean
-}
-
-//3 array afazeres em taredfas.ts
-
-//4
-app.get("/todos", (req: Request, res: Response)=>{
-    // if (tarefas > 0){
-    //     res.send(tarefas)
-    // }
+//retorna todo o array de tarefas = playlists
+app.get("/tarefas", (req, res)=>{
+    res.status(201).send(tarefas)    
 })
+
+//2 e 3 em tarefas.ts
+
+//4 retornar somente os afazeres concluidos
+
+//retorna lista completa de tarefas = playlists by id
+app.get("/tarefas/title", (req, res)=>{
+    const listaTarefas = tarefas.map((tarefa)=>{
+        return tarefa.title;
+    })
+    res.send(listaTarefas)
+})
+
+//retornar tarefas concluídas = playlist by id + condição
+app.get("/tarefas/title/:completed", (req: Request, res: Response)=>{
+
+    const listaTarefaConcluida = tarefas.filter((tarefaFeita)=>{
+        let statusTarefa:any = req.params.completed
+
+        if (statusTarefa === "true"){
+            statusTarefa = true              
+        }else if(statusTarefa === "false"){
+            statusTarefa = false
+            res.status(401).send("Não há tarefa concluida")                
+        }else{
+            res.send("Value de Path Variables deve ser True or False")
+        }
+        return tarefaFeita.completed
+        
+        // const resultado = tarefaFeita.completed
+    })
+    res.status(200).send(listaTarefaConcluida)    
+})
+
+//5 add tarefa
+
+app.post("/adicionarTarefa", (req, res)=>{
+    // const novaTarefa: ToDo[] = req.body
+    const {userId, id, title, completed}: ToDo = req.body
+    
+    const novaTarefa ={
+        userId,
+        id,
+        title,
+        completed
+    }
+
+    tarefas.push(novaTarefa)
+    res.status(201).send(tarefas)    
+})
+
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
--- Modulo4/aprofundamento-express/src/tarefas.ts
+++ Modulo4/aprofundamento-express/src/tarefas.ts
@@ -1,35 +1,40 @@
-import { ToDo } from "./index";
+// import { ToDo } from "./index";
+export type ToDo ={
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean
+};
 
 export const tarefas: ToDo[]=[
-    {
-        "userId": 11,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-      },
-      {
-        "userId": 22,
-        "id": 2,
-        "title": "quis ut nam facilis et officia qui",
-        "completed": false
-      },
-      {
-        "userId": 33,
-        "id": 3,
-        "title": "fugiat veniam minus",
-        "completed": false
-      },
-      {
-        "userId": 44,
-        "id": 4,
-        "title": "et porro tempora",
-        "completed": true
-      },
-      {
-        "userId": 55,
-        "id": 5,
-        "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-        "completed": false
-      }
-
+  {
+  "userId": 11,
+  "id": 1,
+  "title": "estudar",
+  "completed": true,
+},
+{
+  "userId": 22,
+  "id": 2,
+  "title": "se exercitar",
+  "completed": false,
+},
+{
+  "userId": 33,
+  "id": 3,
+  "title": "meditar",
+  "completed": false
+},
+{
+  "userId": 44,
+  "id": 4,
+  "title": "passear com cachorro",
+  "completed": false,
+},
+{
+  "userId": 55,
+  "id": 5,
+  "title": "estudar js",
+  "completed": true,
+},
 ]
