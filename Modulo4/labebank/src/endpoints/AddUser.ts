74c215744c000be70a3eecb41cda52df82ce6502
Renata S. Zamboni <86760783+RenataSZamboni@users.noreply.github.com>
2022-06-06 01:17:08 -0300

    requisitos minimos

--- Modulo4/labebank/src/endpoints/AddUser.ts
+++ Modulo4/labebank/src/endpoints/AddUser.ts
@@ -1,1 +1,25 @@
-//POST
+import { app } from "../index";
+import { Request, Response } from "express";
+import { Usuario, usuarios } from "../data/data";
+
+//5 POST
+app.post("/AddUser",(req: Request, res: Response)=>{
+    const {userId, nome, cpf, nascimento} = req.body
+
+    const novoUsuario: Usuario ={
+        userId,
+        nome,
+        cpf,
+        nascimento
+    }
+    usuarios.push(novoUsuario)
+    res.status(201).send(usuarios)
+
+    //requisição de idade
+    //if (usuario.nascimento >= 18){
+    //     return novoUsuario
+    // }else{
+    //     res.status(400).send("Não cadastramos menores de 18 anos")
+    // }
+    
+})
--- Modulo4/labebank/src/endpoints/GetUsers.ts
+++ Modulo4/labebank/src/endpoints/GetUsers.ts
@@ -1,1 +1,8 @@
-//GET
+import { app } from "../index";
+import { Request, Response } from "express";
+import { Usuario, usuarios } from "../data/data";
+
+//6 GET
+app.get("/getUsers", (req, res)=>{
+    res.status(201).send(usuarios)
+})
