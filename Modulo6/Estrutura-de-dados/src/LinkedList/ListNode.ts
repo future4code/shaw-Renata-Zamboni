fdf9b0a9a6574cb31aa16c2b7a42abeb11e95521
Renata S. Zamboni <86760783+RenataSZamboni@users.noreply.github.com>
2022-07-27 17:03:04 -0300

    Estrutura de Dados

---
+++ Modulo6/Estrutura-de-dados/src/LinkedList/ListNode.ts
@@ -0,0 +1,14 @@
+export const ListNode: string[] = [
+    "Newton",
+    "Bouman",
+    "Sagan",
+    "Hamilton",
+    "Julian",
+    "Melo",
+    "Turing",
+    "Jackson",
+    "Tang",
+    "Dumont"
+]
+ 
+console.log(ListNode);
